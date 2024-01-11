08fd8c63c7c5895ecdb8ac43f44938432b9c7dd8
Bach Bui <bach.bui@mservice.com.vn>
2024-01-11 17:51:15 +0700

    update logo sign in

--- src/views/SignInView.vue
+++ src/views/SignInView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
     <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-      <img class="mx-auto h-10 w-auto" src="http://localhost:5173/logo_vue.png" alt="logo" />
+      <img class="mx-auto h-10 w-auto" src="/logo_vue.png" alt="logo" />
       <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
         {{ $t('signInTitle') }}
       </h2>
